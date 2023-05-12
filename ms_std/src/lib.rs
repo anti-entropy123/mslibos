a52d20376a9bc439e2705a1394ea4ba5242f9d78
anti-entropy123 <44370885+anti-entropy123@users.noreply.github.com>
2023-05-12 15:03:02 +0800

    fix panic_handler error, add should_panic use case. (#14)

--- ms_std/src/lib.rs
+++ ms_std/src/lib.rs
@@ -1,5 +1,4 @@
 #![no_std]
-
 #![feature(lang_items)]
 #![feature(linkage)]
 #![feature(alloc_error_handler)]
@@ -16,25 +15,37 @@ pub mod sync;
 
 #[cfg(feature = "no_std")]
 mod heap_alloc;
-#[cfg(feature = "no_std")]
-use {core::panic::PanicInfo, init_context::isolation_ctx};
+// #[cfg(feature = "no_std")]
+// use {core::panic::PanicInfo, init_context::isolation_ctx};
+use unwinding::panic;
 
-#[cfg(feature = "no_std")]
-#[panic_handler]
-fn panic_handler(_info: &PanicInfo) -> ! {
-    let panic_addr = isolation_ctx().panic_handler;
+// #[cfg(feature = "no_std")]
+// #[panic_handler]
+// fn panic_handler(_info: &PanicInfo) -> ! {
+//     let panic_addr = isolation_ctx().panic_handler;
 
-    let host_panic_handler: unsafe extern "C" fn() -> ! =
-        unsafe { core::mem::transmute(panic_addr) };
-    unsafe { host_panic_handler() }
-}
+//     let host_panic_handler: unsafe extern "C" fn() -> ! =
+//         unsafe { core::mem::transmute(panic_addr) };
+//     unsafe { host_panic_handler() }
+// }
 
-#[cfg(feature = "no_std")]
-#[lang = "eh_personality"]
-extern "C" fn eh_personality() {}
+// #[cfg(feature = "no_std")]
+// #[lang = "eh_personality"]
+// extern "C" fn eh_personality() {}
 
 #[linkage = "weak"]
 #[no_mangle]
-pub fn rust_main() {
-    panic!("need real rust_main");
+pub fn main() {
+    panic!("need real main");
+}
+
+#[no_mangle]
+pub fn rust_main() -> Result<(), ()> {
+    let result = panic::catch_unwind(|| main());
+
+    if let Err(ref e) = result {
+        println!("error: {:#?}", e)
+    }
+
+    result.map_err(|_| ())
 }
