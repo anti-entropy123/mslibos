fb27200729eb7f90ee033c220a8d1aa67cc11ae8
anti-entropy123 <1348651580@qq.com>
2023-11-13 11:35:10 +0800

    extend client.py to access hwh bench
    Signed-off-by: anti-entropy123 <1348651580@qq.com>

--- baseline/map_reduce_rust_openfaas/client.py
+++ baseline/map_reduce_rust_openfaas/client.py
@@ -4,15 +4,19 @@ import json
 import time
 import subprocess
 
-mapper_name = "wc-mapper"
-reducer_name = "wc-reducer"
+mapper_name = "mapper-rust"
+reducer_name = "reducer-rust"
+# mapper_name = "wc-mapper"
+# reducer_name = "wc-reducer"
+gateway = "node-7:9331"
+# gateway = "localhost:31119"
 mapper_num = 5
 reducer_num = 5
 
 
 def invoke_func(func_name: str, data: dict,) -> subprocess.Popen:
     p = subprocess.Popen(
-        ["faas-cli", "-g", "node-7:9331", "invoke", func_name], stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=False)
+        ["faas-cli", "-g", gateway, "invoke", func_name], stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=False)
 
     p.stdin.write(json.dumps(data).encode())
     p.stdin.close()
@@ -28,7 +32,7 @@ def workflow():
         req = {"input_name": "data-500m",
                "input_part": i, "reduce_num": reducer_num}
         handler = invoke_func(
-            "mapper-rust", req)
+            mapper_name, req)
         mapper_handlers.append(handler)
 
     for mapper in mapper_handlers:
@@ -43,7 +47,7 @@ def workflow():
         req = {"input_name": "data-500m",
                "input_num": mapper_num, "reduce_part": i}
         handler = invoke_func(
-            "reducer-rust", req)
+            reducer_name, req)
         reducer_handlers.append(handler)
 
     for reducer in reducer_handlers:
