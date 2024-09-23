aa7ff5f415d18b192583175e7e320137de999140
anti-entropy123 <1348651580@qq.com>
2024-09-23 15:21:19 +0800

    reproduce in helloworld

--- user/hello_world/src/lib.rs
+++ user/hello_world/src/lib.rs
@@ -1,5 +1,6 @@
 #![cfg_attr(feature = "with_libos", no_std)]
 
+use alloc::borrow::ToOwned;
 use ms_std::args;
 
 cfg_if::cfg_if! {
@@ -13,10 +14,21 @@ cfg_if::cfg_if! {
     }
 }
 
+static TEXT: &str = include_str!("../fake_data_0.txt");
+
 #[no_mangle]
 pub fn main() -> Result<()> {
     let id = args::get("id").unwrap();
     println!("Hello, world! id: {}", id);
+
+    // let mut hasher: hashbrown::HashMap<String, u32> = hashbrown::HashMap::new();
+
+    for word in TEXT.split_whitespace() {
+        println!("{}", word)
+        // hasher.insert(word.to_owned(), hasher.get(word).unwrap_or(&0) + 1);
+    }
+    println!("hash insert ok");
+
     #[cfg(feature = "measure_mem")]
     {
         use ms_std::libos::MetricEvent::Mem;
