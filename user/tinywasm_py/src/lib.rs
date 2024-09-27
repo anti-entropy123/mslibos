02a68e13fff0c86b63de2c929e3a8b8d2cc6c1d3
anti-entropy123 <1348651580@qq.com>
2024-09-27 19:36:04 +0800

    impl wasm test cases

--- user/tinywasm_py/src/lib.rs
+++ user/tinywasm_py/src/lib.rs
@@ -11,24 +11,12 @@ cfg_if::cfg_if! {
     }
 }
 
-use alloc::{format, vec};
-use alloc::vec::Vec;
 use ms_std::{
-    libos::{libos},
     println,
     time::{SystemTime, UNIX_EPOCH},
 };
-use wasi_api::tinywasm;
-use tinywasm::Extern;
-use tinywasm::{FuncContext, Imports, Module, Store};
-
-// use ms_std::libos::libos;
-
-#[repr(C)]
-struct WasiCiovec {
-    buf: u32,
-    buf_len: u32,
-}
+use tinywasm::{Module, Store};
+use wasi_api::tinywasm::{self, ModuleInstance};
 
 const WASM: &[u8] = include_bytes!("../rustpython.wasm");
 
@@ -38,18 +26,13 @@ pub fn main(_args: &BTreeMap<String, String>) -> Result<()> {
     let mut store = Store::default();
     let imports = wasi_api::import_all()?;
 
-    let instance = module.instantiate(&mut store, Some(imports))?;
+    let instance = ModuleInstance::instantiate(&mut store, module, Some(imports))?;
     let main = instance.exported_func::<(), ()>(&store, "_start")?;
-    
-    let start_time = SystemTime::now().duration_since(UNIX_EPOCH).as_millis();
 
-    if let Err(e) = unwinding::panic::catch_unwind(|| main.call(&mut store, ()).unwrap()) {
-        let msg = format!("{:?}", e);
-        println!("err msg: {:?}", msg);
-        if msg != "normally exit" {
-            // return Err();
-        }
-    };
+    let _start_time = SystemTime::now().duration_since(UNIX_EPOCH).as_millis();
+
+    let result = unwinding::panic::catch_unwind(|| main.call(&mut store, ()).unwrap());
+    println!("result: {:?}", result);
 
     Ok(().into())
 }
