8ec76950165f16fa77c5476ef063c7d3d8842f1f
tjulym <tjulym@users.noreply.github.com>
2024-09-27 11:31:20 +0800

    Feat/wasm api (#206) add support for py hello

--- wasi_api/src/lib.rs
+++ wasi_api/src/lib.rs
@@ -22,6 +22,31 @@ pub fn import_all() -> tinywasm::Result<Imports> {
         )?
         .define(
             "wasi_snapshot_preview1",
+            "args_get",
+            Extern::typed_func(wasi::args_get),
+        )?
+        .define(
+            "wasi_snapshot_preview1",
+            "args_sizes_get",
+            Extern::typed_func(wasi::args_sizes_get),
+        )?
+        .define(
+            "wasi_snapshot_preview1",
+            "clock_time_get",
+            Extern::typed_func(wasi::clock_time_get),
+        )?
+        .define(
+            "wasi_snapshot_preview1",
+            "environ_get",
+            Extern::typed_func(wasi::environ_get),
+        )?
+        .define(
+            "wasi_snapshot_preview1",
+            "environ_sizes_get",
+            Extern::typed_func(wasi::environ_sizes_get),
+        )?
+        .define(
+            "wasi_snapshot_preview1",
             "fd_close",
             Extern::typed_func(wasi::fd_close),
         )?
@@ -34,6 +59,16 @@ pub fn import_all() -> tinywasm::Result<Imports> {
             "wasi_snapshot_preview1",
             "fd_fdstat_set_flags",
             Extern::typed_func(wasi::fd_fdstat_set_flags),
+        )?
+        .define(
+            "wasi_snapshot_preview1",
+            "fd_filestat_get",
+            Extern::typed_func(wasi::fd_filestat_get),
+        )?
+        .define(
+            "wasi_snapshot_preview1",
+            "fd_filestat_set_size",
+            Extern::typed_func(wasi::fd_filestat_set_size),
         )?
         .define(
             "wasi_snapshot_preview1",
@@ -52,8 +87,23 @@ pub fn import_all() -> tinywasm::Result<Imports> {
         )?
         .define(
             "wasi_snapshot_preview1",
+            "fd_read",
+            Extern::typed_func(wasi::fd_read),
+        )?
+        .define(
+            "wasi_snapshot_preview1",
+            "fd_readdir",
+            Extern::typed_func(wasi::fd_readdir),
+        )?
+        .define(
+            "wasi_snapshot_preview1",
             "fd_seek",
             Extern::typed_func(wasi::fd_seek),
+        )?
+        .define(
+            "wasi_snapshot_preview1",
+            "fd_sync",
+            Extern::typed_func(wasi::fd_sync),
         )?
         .define(
             "wasi_snapshot_preview1",
@@ -62,13 +112,68 @@ pub fn import_all() -> tinywasm::Result<Imports> {
         )?
         .define(
             "wasi_snapshot_preview1",
+            "path_create_directory",
+            Extern::typed_func(wasi::path_create_directory),
+        )?
+        .define(
+            "wasi_snapshot_preview1",
+            "path_link",
+            Extern::typed_func(wasi::path_link),
+        )?
+        .define(
+            "wasi_snapshot_preview1",
             "path_open",
             Extern::typed_func(wasi::path_open),
         )?
         .define(
             "wasi_snapshot_preview1",
+            "path_readlink",
+            Extern::typed_func(wasi::path_readlink),
+        )?
+        .define(
+            "wasi_snapshot_preview1",
+            "path_filestat_get",
+            Extern::typed_func(wasi::path_filestat_get),
+        )?
+        .define(
+            "wasi_snapshot_preview1",
+            "path_filestat_set_times",
+            Extern::typed_func(wasi::path_filestat_set_times),
+        )?
+        .define(
+            "wasi_snapshot_preview1",
+            "path_remove_directory",
+            Extern::typed_func(wasi::path_remove_directory),
+        )?
+        .define(
+            "wasi_snapshot_preview1",
+            "path_rename",
+            Extern::typed_func(wasi::path_rename),
+        )?
+        .define(
+            "wasi_snapshot_preview1",
+            "path_unlink_file",
+            Extern::typed_func(wasi::path_unlink_file),
+        )?
+        .define(
+            "wasi_snapshot_preview1",
+            "poll_oneoff",
+            Extern::typed_func(wasi::poll_oneoff),
+        )?
+        .define(
+            "wasi_snapshot_preview1",
             "proc_exit",
             Extern::typed_func(wasi::proc_exit),
+        )?
+        .define(
+            "wasi_snapshot_preview1",
+            "random_get",
+            Extern::typed_func(wasi::random_get),
+        )?
+        .define(
+            "wasi_snapshot_preview1",
+            "sched_yield",
+            Extern::typed_func(wasi::sched_yield),
         )?;
 
     Ok(imports)
