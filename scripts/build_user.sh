f89201d4d732904d8036913e9f694758702889b4
Chen Yichi <trswnca@yeah.net>
2024-09-29 13:30:40 +0800

    cleaned build files

--- scripts/build_user.sh
+++ scripts/build_user.sh
@@ -6,4 +6,12 @@ else
     feature_arg=""
 fi
 
-find user -name 'Cargo.toml' | xargs -I {} bash -c "cargo build $feature_arg --manifest-path {}"
+find user -name 'Cargo.toml' \
+    -not -path 'user/nn_conv/Cargo.toml' \
+    -not -path 'user/mmap_file/Cargo.toml' \
+    -not -path 'user/httpd/Cargo.toml' \
+    -not -path 'user/should_panic/Cargo.toml' \
+    -not -path 'user/load_all/Cargo.toml' \
+    -not -path 'user/simple_http/Cargo.toml' \
+    -not -path 'user/never_stop/Cargo.toml' | \
+    xargs -I {} bash -c "cargo build $feature_arg --manifest-path {}"
