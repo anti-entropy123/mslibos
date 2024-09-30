f253b816c125b0f9f7923f157ccbc2f2938852dd
Chen Yichi <trswnca@yeah.net>
2024-09-30 16:30:14 +0800

    Fix/Tinywasm py error caused by wasi args set

--- user/tinywasm_py/src/lib.rs
+++ user/tinywasm_py/src/lib.rs
@@ -4,10 +4,9 @@ cfg_if::cfg_if! {
     if #[cfg(feature = "with_libos")] {
         use ms_std::{agent::FaaSFuncResult as Result};
         extern crate alloc;
-        use alloc::{collections::BTreeMap, string::String};
+        use alloc::{string::{ToString, String}, vec::Vec};
     } else {
         type Result<T> = core::result::Result<T, String>;
-        use std::collections::BTreeMap;
     }
 }
 
@@ -25,6 +24,10 @@ pub fn main() -> Result<()> {
     let module = Module::parse_bytes(WASM)?;
     let mut store = Store::default();
     let imports = wasi_api::import_all()?;
+
+    let mut wasi_args: Vec<String> = Vec::new();
+    wasi_args.push("fake system path!".to_string());
+    wasi_api::set_wasi_args(store.id(), wasi_args);
 
     let instance = ModuleInstance::instantiate(&mut store, module, Some(imports))?;
     let main = instance.exported_func::<(), ()>(&store, "_start")?;
