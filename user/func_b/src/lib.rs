ad954bb5de5d2bd001df22a22a879a146da29b62
YJ2378 <73393686+YJ2378@users.noreply.github.com>
2024-10-06 18:36:00 +0800

    Expr/Complete Long_chain and experimental scripts (#221)
    * Expr/long_chain and test scripts finished
    
    ---------
    
    Co-authored-by: Yichi Chen <trswnca@yeah.net>

--- user/func_b/src/lib.rs
+++ user/func_b/src/lib.rs
@@ -8,27 +8,27 @@ use alloc::{borrow::ToOwned, string::String};
 use ms_std::{
     agent::{DataBuffer, FaaSFuncResult as Result},
     println,
-    // time::SystemTime,
+    time::SystemTime,
 };
 use ms_std_proc_macro::FaasData;
 
-const DATA_SIZE: usize = 1;
+const DATA_SIZE: usize = (1024*1024*256-48);
 
 #[derive(FaasData)]
 pub struct MyComplexData {
-    // pub current_time: SystemTime,
+    pub current_time: SystemTime,
     pub year: i64,
     pub name: String,
-    pub big_data: [u8; 4096 * DATA_SIZE],
+    pub big_data: [u8; DATA_SIZE],
 }
 
 impl Default for MyComplexData {
     fn default() -> Self {
         Self {
-            // current_time: SystemTime::now(),
+            current_time: SystemTime::now(),
             year: Default::default(),
             name: Default::default(),
-            big_data: [0; 4096 * DATA_SIZE],
+            big_data: [0; DATA_SIZE],
         }
     }
 }
@@ -39,17 +39,17 @@ pub fn main() -> Result<MyComplexData> {
     println!("func b");
     let data = DataBuffer::<MyComplexData>::from_buffer_slot("Conference".to_owned());
     if let Some(buffer) = data {
-        // let dur = buffer.current_time.elapsed();
         let size = size_of::<MyComplexData>();
-        // for i in 0..buffer.big_data.len() {
-        //     let _ = unsafe { ptr::read_volatile((&buffer.big_data[i]) as *const u8) };
-        // }
+        for i in 0..buffer.big_data.len() {
+            let _ = unsafe { core::ptr::read_volatile((&buffer.big_data[i]) as *const u8) };
+        }
+        let dur = buffer.current_time.elapsed();
         println!(
-            "try recovery data. total_size: {} Bytes",
-            // "try recovery data. trans data time: {:?}, total_size: {} Bytes, transfer rate: {} MB/s",
-            // dur,
+            // "try recovery data. total_size: {} Bytes",
+            "try recovery data. trans data time: {:?}, total_size: {} Bytes, transfer rate: {} MB/s",
+            dur,
             size,
-            // size as u128 / dur.as_micros(),
+            size as u128 / dur.as_micros(),
         );
         println!("{}Sys, {}", buffer.name, buffer.year);
         assert_eq!(buffer.year, 2025);
